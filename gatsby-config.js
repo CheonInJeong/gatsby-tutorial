1bf4380a8e266c6e10458b9a32bb0b68f5f5ecb9
chenchen <burub3@gmail.com>
2022-08-20 15:52:02 +0900

    gatsby plugin 추가(image, sharp, filesystem)

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     title: `My gatsby site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="home page">
       <p>I'm making this by following the Gatsby tutorial.</p>
+      <StaticImage
+        alt="cute cat named dongggeul"
+        src="../images/cute_cat.jpeg"
+      />
     </Layout>
   );
 };
